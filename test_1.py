1b5936d6edd0b7a5638d7f22083aa2c7f284ef59
ardithno <antikyanartem@gmail.com>
2022-07-07 13:56:23 +0300

    test_1.py

---
+++ test_1.py
@@ -0,0 +1,11 @@
+def search_zero(mass):
+    if  isinstance(mass, list) or isinstance(mass, tuple):
+        mass = ''.join([str(i) for i in mass])
+    print(mass.index('0'))
+
+
+search_zero('1111110001111010101111000111')
+search_zero('10')
+search_zero([1, 1, 0, 0])
+search_zero(['1', '1', '1', '0', '1'])
+search_zero((1,1,1,1,0))
