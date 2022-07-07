e9ee1efce84e3fbf300c31ef1989ffc0373eed1c
ardithno <antikyanartem@gmail.com>
2022-07-07 13:58:42 +0300

    test_1.py

--- test_1.py
+++ test_1.py
@@ -4,8 +4,9 @@ def search_zero(mass):
     print(mass.index('0'))
 
 
+'''Tests:
 search_zero('1111110001111010101111000111')
 search_zero('10')
 search_zero([1, 1, 0, 0])
 search_zero(['1', '1', '1', '0', '1'])
-search_zero((1,1,1,1,0))
+search_zero((1,1,1,1,0))'''
