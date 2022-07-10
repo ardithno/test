bfbf61cf8eb55897db3ebab73ea9c1f462fc2d60
ardithno <ardithno@student.21-school.ru>
2022-07-10 18:18:01 +0300

    test_2.py

--- test_2.py
+++ test_2.py
@@ -1,22 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import string
 
-n = 0
+flag = True
 animals  = dict()
 url = "https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту"
 page = requests.get(url).text
 
-while n!=5000:
+while flag:
     soup = BeautifulSoup(page, 'lxml')
     names = soup.find('div', class_='mw-category mw-category-columns').find_all('a')
     for name in names:
+        if name.text[0] == 'A':
+            flag = False
+            break
+        if name.text[0].upper() == 'Ё':
+            animals['Е'] += 1
+            continue
         animals[name.text[0].upper()] = animals.get(name.text[0].upper(), 0) + 1
-        n+=1
     links = soup.find('div', id='mw-pages').find_all('a')
     for a in links:
         if a.text == 'Следующая страница':
             url = 'https://ru.wikipedia.org/' + a.get('href')
             page = requests.get(url).text
 
-for key, value in animals.items.sorted():
-    print(f'{key}: {value}')
+for key in sorted(animals):
+    if key in string.ascii_uppercase:
+        continue
+    print(f'{key}: {animals[key]}')
